092631b170307bc685085355578fe823f3a094ee
Akhil Vikraman <akhilvikraman@Akhils-MacBook-Pro.local>
2026-01-26 12:38:03 +0000

    Add https

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://vsakhilvs.pythonanywhere.com',
+        target: 'https://vsakhilvs.pythonanywhere.com',
         changeOrigin: true,
       }
     }
